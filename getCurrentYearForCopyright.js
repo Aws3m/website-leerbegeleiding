badf95585f0f86bef5ad3f5cec50c17741598329
SemRadoes <semradijs@hotmail.com>
2024-09-17 23:25:58 +0200

    further improvements

--- getCurrentYearForCopyright.js
+++ getCurrentYearForCopyright.js
@@ -1,4 +1,2 @@
-const copyrightSign = document.createElement("p");
 const year = new Date().getFullYear();
-copyrightSign.innerHTML = "<div class='copyrightfooter'>&copy; " + year + "<span style='font-size: .8em; text-decoration: underline;'>privacy policy</span></div>";
-$('#copyright').append(copyrightSign);
+$('#copyright').append("<div class='copyrightfooter'>&copy; " + year + "<span style='font-size: .8em; text-decoration: underline;'>privacy policy</span></div>");
