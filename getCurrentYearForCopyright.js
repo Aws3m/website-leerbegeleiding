d5c87b54de5b79d56c95bd44a8d0c311480243ce
SemRadoes <semradijs@hotmail.com>
2024-09-30 22:18:19 +0200

    working on individual pages

--- getCurrentYearForCopyright.js
+++ getCurrentYearForCopyright.js
@@ -1,2 +1,2 @@
 const year = new Date().getFullYear();
-$('#copyright').append("<div class='copyrightfooter'>&copy; " + year + "<span style='font-size: .8em; text-decoration: underline;'>privacy policy</span></div>");
+$('#copyright').append("<div class='copyrightfooter'>Designed and built by Awsem webdesign &copy; " + year + "<span style='font-size: .8em; text-decoration: underline;'>privacy policy</span></div>");
